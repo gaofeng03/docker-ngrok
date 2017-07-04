8c813341d141f028602a9c78edd7767bccca9f22
lewis <lewis@Lewis-MacBook.local>
2017-07-04 17:56:30 +0800

    sh permission

--- ngrok-server.sh
+++ ngrok-server.sh
@@ -5,18 +5,18 @@ ngrokd_pid="`ps -ef | grep -v grep | grep ngrokd | sed -n  '1P' | awk '{print $2
 
 #start ngrokd server
 function start {
-    /root/ngrok/bin/ngrokd -domain="$NGROK_HOST" -httpAddr=":$NGROK_HTTP_ADDR" -httpsAddr=":$NGROK_HTTPS_ADDR"
+    bash /root/ngrok/bin/ngrokd -domain="$NGROK_HOST" -httpAddr=":$NGROK_HTTP_ADDR" -httpsAddr=":$NGROK_HTTPS_ADDR"
     echo "ngrokd start"
 }
 
 #build ngrokd server & client
 function build {
-    /root/ngrok-build.sh
+    bash /root/ngrok-build.sh
     echo "ngrokd build"
 }
 
 function watchdog {
-    /root/watchdog.sh /root/watchdog-server.txt
+    bash /root/watchdog.sh /root/watchdog-server.txt
 }
 
 #ngrokd 是否存在
