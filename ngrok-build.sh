27426825290682048ac2e9c2f80434c4a126fc99
lewis <lewis@Lewis-MacBook.local>
2017-07-04 18:16:28 +0800

    server

--- ngrok-build.sh
+++ ngrok-build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-# 2. cert
+
+# 1. work dir
 cd /root/ngrok
+
+# 2. cert
 mkdir cert
 cd /root/ngrok/cert
 openssl genrsa -out rootCA.key 2048
@@ -8,13 +11,16 @@ openssl req -x509 -new -nodes -key rootCA.key -subj "/CN=$NGROK_HOST" -days 5000
 openssl genrsa -out device.key 2048
 openssl req -new -key device.key -subj "/CN=$NGROK_HOST" -out device.csr
 openssl x509 -req -in device.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out device.crt -days 5000
+
 # 3. key
 cp rootCA.pem /root/ngrok/assets/client/tls/ngrokroot.crt
 cp device.crt /root/ngrok/assets/server/tls/snakeoil.crt
 cp device.key /root/ngrok/assets/server/tls/snakeoil.key
+
 # 4. make
 cd /root/ngrok
 make release-server release-client
 cp /root/ngrok/bin/ngrok /usr/ngrok
+
 # 5. run
 # /root/ngrok/bin/ngrokd -domain="$NGROK_HOST" -httpAddr=":$NGROK_HTTP_ADDR" -httpsAddr=":$NGROK_HTTPS_ADDR"
--- ngrok-server.sh
+++ ngrok-server.sh
@@ -3,37 +3,31 @@
 ngrokd_file="/root/ngrok/bin/ngrokd"
 ngrokd_pid="`ps -ef | grep -v grep | grep ngrokd | sed -n  '1P' | awk '{print $2}'`"
 
-#start ngrokd server
+# start ngrokd server
 function start {
     /root/ngrok/bin/ngrokd -domain="$NGROK_HOST" -httpAddr=":$NGROK_HTTP_ADDR" -httpsAddr=":$NGROK_HTTPS_ADDR"
     echo "ngrokd start"
 }
 
-#build ngrokd server & client
+# build ngrokd server & client
 function build {
     bash /root/ngrok-build.sh
     echo "ngrokd build"
 }
 
+# 守护进程
 function watchdog {
     bash /root/watchdog.sh /root/watchdog-server.txt
 }
 
-#ngrokd 是否存在
-if [ -e "$ngrokd_file" ] && [ -z "$ngrokd_pid" ] ; then
-    echo "ngrokd is exist"
-    start
-    watchdog
-else
+# ngrokd 是否存在
+if [ ! -e "$ngrokd_file" ] ; then
     echo "no ngrokd"
     build
-fi
-
-#ngrokd 是否运行
-if [ -e "$ngrokd_file" ] && [ -z "$ngrokd_pid" ] ; then
+    start
+    watchdog
+elif [ -z "$ngrokd_pid" ] ; then
     echo "no this process"
     start
     watchdog
-else
-    echo "ngrokd is runing"
 fi
