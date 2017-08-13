5955983350e92a29657911d690c93c92fe51dbc7
lewis <lewis@Lewis-MacBook.local>
2017-08-13 11:11:31 +0800

    nohup

--- ngrok-ssh.sh
+++ ngrok-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ngrok client start
-/root/ngrok/ngrok -config=/root/ngrok/ngrok.cfg start-all
+nohup /root/ngrok/ngrok -config=/root/ngrok/ngrok.cfg start-all &
 
 # 守护进程
 #bash /root/ngrok/watchdog.sh /root/ngrok/watchdog-client.txt
